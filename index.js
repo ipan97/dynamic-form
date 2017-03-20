dea4d235e5b5746c82725c6c7d28b26ecaa57041
Ipan Taupik Rahman <ipan.rahman@wgs.co.id>
2017-03-20 15:00:45 +0700

    dynamic form with jquery and bootstrap

---
+++ index.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	var i = $('input').length + 1;
+	$('#add').click(function() {
+		$('<div><input type="text" class="field" name="dynamic[]" value="' + i + '" /></div>').fadeIn('slow').appendTo('.inputs');
+		i++;
+	});
+	$('#remove').click(function() {
+	if(i > 1) {
+		$('.field:last').remove();
+		i--; 
+	}
+	});
+	$('#reset').click(function() {
+	while(i > 2) {
+		$('.field:last').remove();
+		i--;
+	}
+	});
+	$('.submit').click(function(){						
+	var answers = [];
+    $.each($('.field'), function() {
+        answers.push($(this).val()); 
+    });
+    if(answers.length == 0) { 
+        answers = "none"; 
+    }   
+	alert(answers);
+	
+	return false;						
+	});
+});
